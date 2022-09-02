f1e8bf8dfc1671f92fd4c0b10865c7628f0542f1
three97-dev <vitalii.nobis@neatbyte.solutions>
2022-09-02 07:48:53 +0000

    Fix image height on Careers page

--- src/components/careers/News.js
+++ src/components/careers/News.js
@@ -20,17 +20,17 @@ const News = ({
         <div className="flex flex-col h-full text-dark-gray">
           <h3 className="text-[24px] leading-[29px] font-inter font-bold text-center mb-7">{firstNewsTitle}</h3>
           <p className="md:mb-10 leading-[22px] font-inter">{firstNewsDescription}</p>
-          <Image className="block mt-auto mx-auto" image={firstNewsImage} />
+          <Image className="block mt-auto mx-auto md:h-[200px] lg:h-[270px]" image={firstNewsImage} />
         </div>
         <div className="flex flex-col h-full text-dark-gray">
           <h3 className="text-[24px] leading-[29px] font-inter font-bold text-center mb-7">{secondNewsTitle}</h3>
           <p className="md:mb-10 leading-[22px] font-inter">{secondNewsDescription}</p>
-          <Image className="block mt-auto mx-auto" image={secondNewsImage} />
+          <Image className="block mt-auto mx-auto md:h-[200px] lg:h-[270px]" image={secondNewsImage} />
         </div>
         <div className="flex flex-col h-full text-dark-gray">
           <h3 className="text-[24px] leading-[29px] font-inter font-bold text-center mb-7">{thirdNewsTitle}</h3>
           <p className="md:mb-10 leading-[22px] font-inter">{thirdNewsDescription}</p>
-          <Image className="block mt-auto mx-auto" image={thirdNewsImage} />
+          <Image className="block mt-auto mx-auto md:h-[200px] lg:h-[270px]" image={thirdNewsImage} />
         </div>
       </div>
       <div className="md:hidden mt-5">
@@ -38,17 +38,17 @@ const News = ({
           <div className="!grid gap-y-7 h-full text-dark-gray">
             <h3 className="text-[24px] leading-[29px] font-inter font-bold">{firstNewsTitle}</h3>
             <p className="md:mb-5 leading-[22px] font-inter">{firstNewsDescription}</p>
-            <Image className="self-end mt-auto mx-auto" image={firstNewsImage} />
+            <Image className="self-end mt-auto mx-auto w-full max-w-[385px] h-[270px]" image={firstNewsImage} />
           </div>
           <div className="!grid gap-y-7 h-full text-dark-gray">
             <h3 className="text-[24px] leading-[29px] font-inter font-bold">{secondNewsTitle}</h3>
             <p className="md:mb-5 leading-[22px] font-inter">{secondNewsDescription}</p>
-            <Image className="self-end mt-auto mx-auto" image={secondNewsImage} />
+            <Image className="self-end mt-auto mx-auto w-full max-w-[385px] h-[270px]" image={secondNewsImage} />
           </div>
           <div className="!grid gap-y-7 h-full text-dark-gray">
             <h3 className="text-[24px] leading-[29px] font-inter font-bold">{thirdNewsTitle}</h3>
             <p className="md:mb-5 leading-[22px] font-inter">{thirdNewsDescription}</p>
-            <Image className="self-end mt-auto mx-auto" image={thirdNewsImage} />
+            <Image className="self-end mt-auto mx-auto w-full max-w-[385px] h-[270px]" image={thirdNewsImage} />
           </div>
         </Slider>
       </div>
